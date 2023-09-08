f97a341dbfd7ad0a42aa21fc49a2be93ad10bebb
08Miguel24 <MichaelAveuc571@gmail.com>
2023-09-08 19:22:06 +0800

    update

--- client-side/src/components/Hero.svelte
+++ client-side/src/components/Hero.svelte
@@ -1,7 +1,7 @@
 <section class="hero">
     <div class="greetings-container">
         <h1 class="eng-greeting">Warm greetings to you my friend, my name is Michael.</h1>
-        <p class="russian-greeting"></p>
+        <p class="rus-greeting"></p>
     </div>
     <div class="socials">
         socials here
