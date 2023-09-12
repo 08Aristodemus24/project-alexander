4e7e1d7f934178c35f7796f1b3d0da7052be64eb
08Miguel24 <MichaelAveuc571@gmail.com>
2023-09-12 12:25:15 +0800

    update

--- client-side/src/components/Hero.svelte
+++ client-side/src/components/Hero.svelte
@@ -3,6 +3,7 @@
     export let section = "landing-section";
 
     // ratio of facebook and linkedin svg is 1 to 1.43
+    // 36 to 25 for these, and 30 to 30 for others
 </script>
 
 <section id={section} class="hero">
@@ -12,8 +13,6 @@
     </div>
     <div class="socials">
         <svg 
-            width="30" 
-            height="30"
             viewBox="-2.5 0 19 19" 
             xmlns="http://www.w3.org/2000/svg" 
             class="github-icon"
@@ -21,8 +20,6 @@
             <path d="M9.464 17.178a4.506 4.506 0 01-2.013.317 4.29 4.29 0 01-2.007-.317.746.746 0 01-.277-.587c0-.22-.008-.798-.012-1.567-2.564.557-3.105-1.236-3.105-1.236a2.44 2.44 0 00-1.024-1.348c-.836-.572.063-.56.063-.56a1.937 1.937 0 011.412.95 1.962 1.962 0 002.682.765 1.971 1.971 0 01.586-1.233c-2.046-.232-4.198-1.023-4.198-4.554a3.566 3.566 0 01.948-2.474 3.313 3.313 0 01.091-2.438s.773-.248 2.534.945a8.727 8.727 0 014.615 0c1.76-1.193 2.532-.945 2.532-.945a3.31 3.31 0 01.092 2.438 3.562 3.562 0 01.947 2.474c0 3.54-2.155 4.32-4.208 4.548a2.195 2.195 0 01.625 1.706c0 1.232-.011 2.227-.011 2.529a.694.694 0 01-.272.587z"/>
         </svg>
         <svg 
-            width="30" 
-            height="30" 
             viewBox="0 0 32 32" 
             xmlns="http://www.w3.org/2000/svg"
             class="kaggle-icon"
@@ -32,8 +29,6 @@
         <svg 
             version="1.0" 
             xmlns="http://www.w3.org/2000/svg" 
-            width="30" 
-            height="30" 
             viewBox="0 0 1008 1008"
             class="stackoverflow-icon"
         >
@@ -44,8 +39,6 @@
         <svg 
             version="1.0" 
             xmlns="http://www.w3.org/2000/svg" 
-            width="36" 
-            height="25"
             viewBox="0 0 1440 1008"
             class="facebook-icon"
         >
@@ -54,8 +47,6 @@
         <svg 
             version="1.0" 
             xmlns="http://www.w3.org/2000/svg" 
-            width="36" 
-            height="25" 
             viewBox="0 0 1440 1008"
             class="linkedin-icon"
         >
