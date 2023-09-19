f4dd2b68972d189c9c7f184ce563d99d935db1a6
08Miguel24 <MichaelAveuc571@gmail.com>
2023-09-19 20:52:36 +0800

    created initial stylesheet for ugly but responsive accordion

--- client-side/src/components/Projects.svelte
+++ client-side/src/components/Projects.svelte
@@ -6,7 +6,7 @@
         </div>
         <div class="projects-accordion-container">
             <div class="accordion-group">
-                <div class="accordion-item">
+                <div class="accordion-item" data-background="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/dummy1.png">
                     <button class="item-toggler" aria-expanded="true" aria-controls="accordion-content">
                         Accordion #1
                         <svg class="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 
-                <div class="accordion-item">
+                <div class="accordion-item" data-background="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/dummy2.png">
                     <button class="item-toggler" aria-expanded="true" aria-controls="accordion-content">
                         Accordion #2
                         <svg class="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -34,7 +34,7 @@
                     </div>
                 </div>
                 
-                <div class="accordion-item">
+                <div class="accordion-item" data-background="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/Default_lady_justice_sunset_background_wormseye_view_perspecti_0_f49a15e9-180d-4205-9a2d-d3efe582958f_1.jpg">
                     <button class="item-toggler" aria-expanded="true" aria-controls="accordion-content">
                         Accordion #3
                         <svg class="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -48,7 +48,7 @@
                     </div>
                 </div>
 
-                <div class="accordion-item">
+                <div class="accordion-item" data-background="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/ant%20swarm%20for%20breast-cancer-classifier%20upscaled.jpg">
                     <button class="item-toggler" aria-expanded="true" aria-controls="accordion-content">
                         Accordion #4
                         <svg class="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -62,7 +62,7 @@
                     </div>
                 </div>
 
-                <div class="accordion-item">
+                <div class="accordion-item" data-background="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/Default_far_out_side_view_statue_of_Alexander_The_Great_conque_1_76c1007c-b966-40e0-a74f-9ad4dc8bcec8_1.jpg">
                     <button class="item-toggler" aria-expanded="true" aria-controls="accordion-content">
                         Accordion #5
                         <svg class="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -76,7 +76,7 @@
                     </div>
                 </div>
 
-                <div class="accordion-item">
+                <div class="accordion-item" data-background="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/hate-speech-classifier%20thumbnail%20final.png">
                     <button class="item-toggler" aria-expanded="true" aria-controls="accordion-content">
                         Accordion #6
                         <svg class="hs-accordion-active:hidden hs-accordion-active:text-blue-600 hs-accordion-active:group-hover:text-blue-600 block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
