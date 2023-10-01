8a918ab291b7dcde93d4a67433f1f4390d880155
08Miguel24 <MichaelAveuc571@gmail.com>
2023-10-01 21:23:31 +0800

    built final skills animations, and rendered hd video

--- client-side/src/components/About.svelte
+++ client-side/src/components/About.svelte
@@ -5,5 +5,8 @@
             <p>I am a currently a student specializing in machine learning engineering and an aspiring computational immunologist</p>
             <p>Here's some technologies I picked up</p>
         </div>
+        <div class="anims-container">
+            <video autoplay muted loop src="../mediafiles/Skillsfinal.mp4"></video>
+        </div>
     </div>
 </section>
