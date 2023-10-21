e9c267772935211d3efa0866cf258f7a72eaa9e5
08Miguel24 <MichaelAveuc571@gmail.com>
2023-10-21 20:38:07 +0800

    update

--- client-side/src/components/Timeline.svelte
+++ client-side/src/components/Timeline.svelte
@@ -12,49 +12,28 @@
         {year: '2025', title: 'AI Researcher', organization: 'Deep Mind', accolades: ["Lead a team of ML researchers in conducting an experiment of testing novel language model architectures"]},
     ];
 
-    // initially define width and height offsets as null for this
-    // will be changed as size of the timeline-item header changes
-    let header_size_offsets = exp_descriptions.map(() => ({offset_width: null, offset_height: null}));
-    let accolades_size_offsets = exp_descriptions.map(() => ({offset_width: null, offset_height: null}));
-    
-    let header_max_width = null;
-    let header_max_height = null;
-    let accolades_max_width = null;
-    // let accolades_max_height = null;
+    // take the current width of the timeline container
+    let timeline_container_width = null;
 
+    // on mount or after update 
     afterUpdate(() => {
-        /* 
-        because upon mounting binded properties to state variables are triggered 
-        in the carousel items and therefore triggeres a state update we also
-        calculate right then adn there the maximum of the offset heights and widths
-        in the newly updated size_offsets array
-        again after changing state of dynamic max content and fit content values
-        of height and width by resizing window calculate the max height and width
-        and change the previous max height and width states 
-        */
-
-        header_max_height = Math.max(...header_size_offsets.map((timeline_item_header_offset) => timeline_item_header_offset['offset_height']));
-        header_max_width = Math.max(...header_size_offsets.map((timeline_item_header_offset) => timeline_item_header_offset['offset_width']));
-        // // accolades_max_height = Math.max(...accolades_size_offsets.map((timeline_item_accolades_offset) => timeline_item_accolades_offset['offset_height']));
-        accolades_max_width = Math.max(...accolades_size_offsets.map((timeline_item_accolades_offset) => timeline_item_accolades_offset['offset_width']));
+        console.log(timeline_container_width);
     });
 </script>
-<div class="timeline-container">    
+
+<svelte:window on:resize={() => {
+    const timeline_container = document.querySelector('.timeline-container');
+    timeline_container_width = timeline_container.scrollWidth;
+}}/>
+
+<div class="timeline-container" data-scroll-width={timeline_container_width}>
     {#each exp_descriptions as exp_desc, index}
+        <!-- all odd numbered experiences will have its header on the end of the vertical axis and start of the horizontal axis -->
         <div 
             class="header" class:up={index % 2 === 0} class:down={index % 2 !== 0}
-            style:min-height={`${header_max_height}px`}
-            style:width={`${header_max_width}px`}
             style:grid-row={index % 2 === 0 ? `${1} / ${2}` : `${3} / ${4}`}
             style:grid-column={`${index + 1} / ${index + 2}`}
-            style:align-self={index % 2 === 0 ? `end` : `start`}
-            
-            bind:offsetWidth={header_size_offsets[index].offset_width} 
-            bind:offsetHeight={header_size_offsets[index].offset_height}
         >
-        {header_size_offsets[index].offset_width}
-        {header_size_offsets[index].offset_height}
-        {header_max_width}
             <h3 class="title">{exp_descriptions[index]['title']}</h3>
             <h5 class="organization">{exp_descriptions[index]['organization']}</h5>
             {#if exp_descriptions[index]['year'] !== undefined}
@@ -68,12 +47,8 @@
         ></div>
         <ul
             class="accolades" class:up={index % 2 === 0} class:down={index % 2 !== 0}
-            style:width={`${accolades_max_width}px`} 
             style:grid-row={index % 2 === 0 ? `${3} / ${4}` : `${1} / ${2}`}
             style:grid-column={`${index + 1} / ${index + 2}`}
-            style:align-self={index % 2 === 0 ? `start` : `end`}
-            bind:offsetWidth={accolades_size_offsets[index].offset_width} 
-            bind:offsetHeight={accolades_size_offsets[index].offset_height}
         >
             {#each exp_desc['accolades'] as accolade}
                 <li class="accolade">{accolade}</li>
