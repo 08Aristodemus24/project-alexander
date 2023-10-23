59764391441be6153a2510507ffe25b14bcaf031
08Miguel24 <MichaelAveuc571@gmail.com>
2023-10-23 21:56:48 +0800

    building button animation for contributions

--- client-side/src/components/Experience.svelte
+++ client-side/src/components/Experience.svelte
@@ -66,7 +66,7 @@
         <Timeline/>
         <ContribsButton/>
         <a class="cv-button" href="https://drive.google.com/uc?export=download&id=1s36RyoYfjZOtEAxKcF4hchcIuXBYdAzu" download="Larry Miguel R. Cueva Resume">
-            <span class="cv-header-text">Download my cv</span>
+            <span class="cv-header">Download my cv</span>
             <span class="download-icon"></span>
         </a>
     </div>
