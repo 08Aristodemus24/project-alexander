83b68f67f2ddd455490d0e57f2431dcbd8ac0a45
08Miguel24 <MichaelAveuc571@gmail.com>
2023-10-23 09:40:40 +0800

    building button animation for contributions

---
+++ client-side/src/components/ContribsButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    
+</script>
+<button class="contribs-header">
+    View Github contributions
+    <table>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</button>
