c48f1cc22886fa9cf96258a96a3cab11094724fe
08Miguel24 <MichaelAveuc571@gmail.com>
2023-10-28 20:28:30 +0800

    built and finished experience component and footer component

--- client-side/src/components/Footer.svelte
+++ client-side/src/components/Footer.svelte
@@ -1,11 +1,10 @@
 <footer class="footer-container">
-    <div class="footer">
-        <p class="footer-credits">UI/UX consultation and design by Lara Marie R. Cueva. Connect with her via <a href="https://ph.linkedin.com/in/lara-marie-cueva-423925248">LinkedIn</a>.</p>
-        <p class="footer-end">
-            <span class="footer-brand">Michael</span>
-            <span class="footer-text">
-                © <span class="footer-year">2023</span>. All rights reserved.
-            </span>
-        </p>
+    <div class="footer-content">
+        <div class="wrapper">
+            <p class="footer-credits">UI/UX consultation and design by Lara Marie R. Cueva. Connect with her via <a href="https://ph.linkedin.com/in/lara-marie-cueva-423925248">LinkedIn</a>.</p>
+            <p class="footer-end">
+                <span class="footer-year">2023</span> © by Michael Cueva. All rights reserved.
+            </p>
+        </div>
     </div>
 </footer>
