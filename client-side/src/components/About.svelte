5460685ab005d0d092e857413d4442b0400d31ce
08Miguel24 <MichaelAveuc571@gmail.com>
2023-11-08 17:54:15 +0800

    update

--- client-side/src/components/About.svelte
+++ client-side/src/components/About.svelte
@@ -29,7 +29,7 @@
 
             {#if is_mounted === true}
                 <video autoplay muted loop>
-                    <source src="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/assets/mediafiles/Skillsfinal.mp4" type="video/mp4">
+                    <source src="https://raw.githubusercontent.com/08Aristodemus24/project-alexander/master/client-side/src/boards/compressed%20images/Skillsfinal.mp4" type="video/mp4">
                 </video>
             {/if}
         </div>
