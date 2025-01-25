428607fcf3a536def5fa96b66457ca473173b410
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-01-25 18:27:41 +0800

    updated from shell

--- client-side/src/components/OuterCarousel.svelte
+++ client-side/src/components/OuterCarousel.svelte
@@ -5,7 +5,8 @@
     
     <h1 class="main-title" class:active={curr_index === 0}>Skills</h1>
     <span class="ml-skills-title" class:active={curr_index === 1}>Machine Learning & Deep Learning</span>
-    <span class="dva-skills-title" class:active={curr_index === 2}>Data Visualization & Analysis</span>
-    <span class="csswd-skills-title" class:active={curr_index === 3}>Client & Server Side Web Development</span>
-    <span class="misc-skills-title" class:active={curr_index === 4}>Miscellaenous</span>
+    <span class="de-skills-title" class:active={curr_index === 2}>Data Engineering</span>
+    <span class="dva-skills-title" class:active={curr_index === 3}>Data Visualization & Analysis</span>
+    <span class="csswd-skills-title" class:active={curr_index === 4}>Client & Server Side Web Development</span>
+    <span class="misc-skills-title" class:active={curr_index === 5}>Miscellaenous</span>
 </div>
